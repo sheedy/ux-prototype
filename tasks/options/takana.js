6416d1ec8ab8002311d40634c3f3a152d350f9af
Michael Sheedy <git@michaelsheedy.com>
2014-04-09 19:45:36 -0400

    Adding takana but not working as yet

---
+++ tasks/options/takana.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+
+
+	// options: {
+	// 	includePaths: ["src/css/*.scss", ".tmp/css/", "src/bower_components", ".tmp/"],
+	// 	path: "src/css"
+	// }
+	// options: {
+	// 	loadPath: ["src/bower_components"],
+	// },
+	// server: {
+	// 	options: {							// Target options
+	// 		style: 'expanded', // nested | compact | compressed | expanded
+	// 		precision: 6,
+	// 		// trace: false,
+	// 		// sourcemap: false,
+	// 		// debugInfo: false,
+	// 		// lineNumbers: false,
+	// 		loadPath: ["src/bower_components"],
+	// 		// // require: "susy",
+	// 		// bundleExec: false,
+	// 		// // banner: "/* Test banner */", // Can't be used if you use the sourcemap option.
+	// 		path: "src/css"
+	// 	},
+	//     files: {
+	//       '.tmp/css/styles.css' : 'src/css/*.scss'
+	//     }
+	// }
+}
