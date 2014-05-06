af5688012e8808f8446203013d41c076ddb06530
Michael Sheedy <git@michaelsheedy.com>
2014-05-06 00:27:36 -0400

    Gruntfile, bower and NPM updates
    - prevent load-grunt-config from taking over task loading from jit-grunt
    - added ruler-guides
    - added grunt-svg-sprite
    - added ability for JS to detect media queries

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 
+    // Source: http://adactio.com/journal/5429/
+    var size = window.getComputedStyle(document.body,':after').getPropertyValue('content');
+
+    if (size == 'widescreen') {
+        // Load some more content.
+    }
+
+    // If you’re finding that some browsers are including the quotes in the returned :after value, try changing
+    if (size == 'widescreen')  {}
+    // to
+    if (size.indexOf("widescreen") !=-1) {}
+
 });
